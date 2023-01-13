906c6298c36b54c2c19517833ccf134629b11bf3
CELINAJURE <celinajure@gmail.com>
2023-01-13 10:27:07 -0300

    proyecto13 entero, primer commit

---
+++ src/main/java/com/JURE/SB13CELINA/Sb13CelinaApplication.java
@@ -0,0 +1,13 @@
+package com.JURE.SB13CELINA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb13CelinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb13CelinaApplication.class, args);
+	}
+
+}
